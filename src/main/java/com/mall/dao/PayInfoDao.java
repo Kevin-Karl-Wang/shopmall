92507e0e2f704597b1c2d1f6ee6f371bb82edc83
wangyf9 <wangyf9@asiainfo.com>
2017-07-12 23:19:59 +0800

    第一次更新

---
+++ src/main/java/com/mall/dao/PayInfoDao.java
@@ -0,0 +1,17 @@
+package com.mall.dao;
+
+import com.mall.model.PayInfo;
+
+public interface PayInfoDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(PayInfo record);
+
+    int insertSelective(PayInfo record);
+
+    PayInfo selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(PayInfo record);
+
+    int updateByPrimaryKey(PayInfo record);
+}
---
+++ src/main/java/com/mall/dao/ShippingDao.java
@@ -0,0 +1,17 @@
+package com.mall.dao;
+
+import com.mall.model.Shipping;
+
+public interface ShippingDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
